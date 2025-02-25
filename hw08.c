1151f75999ddad9803b14a4979f8e939ba54b7ca
Aadi Rave <arave@purdue.edu>
2025-02-25 15:14:39 -0500

    finished

--- hw08.c
+++ hw08.c
@@ -16,19 +16,77 @@
 // expr/expr_rest, term/term_rest, or factor
 
 #ifndef INSTRUCTOR_EXPR
-enum parse_result parse_expr(struct stream *s) { return PARSE_SUCCESS; }
+enum parse_result parse_expr(struct stream *s) {
+  if (parse_term(s) == PARSE_FAILURE) {
+    return PARSE_FAILURE;
+  }
 
-enum parse_result parse_expr_rest(struct stream *s) { return PARSE_SUCCESS; }
+  if (parse_expr_rest(s) == PARSE_FAILURE) {
+    return PARSE_FAILURE;
+  }
+
+  return PARSE_SUCCESS;
+}
+
+enum parse_result parse_expr_rest(struct stream *s) {
+  struct token peeked = peek(s);
+
+  if (peeked.type == TOK_BINOP &&
+      (peeked.start[0] == '+' || peeked.start[0] == '-')) {
+    next(s);
+    return parse_expr(s);
+  }
+
+  return PARSE_SUCCESS;
+}
 #endif
 
 #ifndef INSTRUCTOR_TERM
-enum parse_result parse_term(struct stream *s) { return PARSE_SUCCESS; }
+enum parse_result parse_term(struct stream *s) {
+  if (parse_factor(s) == PARSE_FAILURE) {
+    return PARSE_FAILURE;
+  }
 
-enum parse_result parse_term_rest(struct stream *s) { return PARSE_SUCCESS; }
+  if (parse_term_rest(s) == PARSE_FAILURE) {
+    return PARSE_FAILURE;
+  }
+
+  return PARSE_SUCCESS;
+}
+
+enum parse_result parse_term_rest(struct stream *s) {
+  struct token peeked = peek(s);
+
+  if (peeked.type == TOK_BINOP && peeked.start[0] == '*') {
+    next(s);
+    return parse_term(s);
+  }
+
+  return PARSE_SUCCESS;
+}
 #endif
 
 #ifndef INSTRUCTOR_FACTOR
-enum parse_result parse_factor(struct stream *s) { return PARSE_FAILURE; }
+enum parse_result parse_factor(struct stream *s) {
+  struct token peeked = peek(s);
+
+  if (peeked.type == TOK_LITERAL) {
+    next(s);
+    return PARSE_SUCCESS;
+  } else if (peeked.type == TOK_LPAREN) {
+    next(s);
+    if (parse_expr(s) == PARSE_FAILURE) {
+      return PARSE_FAILURE;
+    }
+
+    if (next(s).type != TOK_RPAREN) {
+      return PARSE_FAILURE;
+    }
+
+    return PARSE_SUCCESS;
+  }
+  return PARSE_FAILURE;
+}
 #endif
 
 enum parse_result is_valid_expr(const char *expr) {
